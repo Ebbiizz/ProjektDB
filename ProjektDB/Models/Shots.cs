e284611de543c1b85d75f949817f59cc7f3e1dd1
Ebba Högdahl <id22ehl@cs.umu.se>
2024-12-19 10:57:00 +0100

    Added and finished Shots modell

---
+++ ProjektDB/Models/Shots.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjektDB.Models
+{
+    public class Shots
+    {
+        public Shots() { }
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public int ShooterId { get; set; }
+        public int TargetX { get; set; }
+        public int TargetY {  get; set; }
+        public Boolean Hit {  get; set; }
+        public DateTime ShotTime { get; set; }
+    }
+}
