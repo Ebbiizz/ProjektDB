b75717974531b1c587865fa13c35872511916717
Ebba Högdahl <id22ehl@cs.umu.se>
2024-12-19 11:05:17 +0100

    Added and finished ships modell and added shiptype enum

--- ProjektDB/Models/EnumModells.cs
+++ ProjektDB/Models/EnumModells.cs
@@ -6,4 +6,12 @@
         Paused,
         Ended
     }
+    public enum ShipType
+    {
+        Carrier, //5
+        Battleship, //4
+        Cruiser, //3
+        Submarine, //3
+        Destroyer //2
+    }
 }
---
+++ ProjektDB/Models/Ships.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjektDB.Models
+{
+    public class Ships
+    {
+        public Ships() { }
+        public int Id { get; set; }
+        public ShipType Type { get; set; }
+        public int StartX { get; set; }
+        public int StartY { get; set; }
+        public int EndX { get; set; }
+        public int EndY { get; set; }
+    }
+}
