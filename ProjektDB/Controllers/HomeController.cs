f9b199a7d1d9b16e2975449c38c1d38c92468b16
lisj0130 <lisj0130@student.umu.se>
2024-12-19 10:34:13 +0100

    Testing

--- ProjektDB/Controllers/HomeController.cs
+++ ProjektDB/Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace ProjektDB.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //Test
     }
 }
