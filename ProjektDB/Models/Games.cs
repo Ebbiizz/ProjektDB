47dc5756767d47fdfaa9e151a857f1920699d639
Ebba Högdahl <id22ehl@cs.umu.se>
2024-12-19 10:37:29 +0100

    Added modell

---
+++ ProjektDB/Models/Games.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjektDB.Models
+{
+    public class Games
+    {
+        public Games() { }
+        public int Id { get; set; }
+        public int Player1Id { get; set; }
+        public int Player2Id { get; set; }
+        public int CurrentTurn { get; set; }
+        public DateTime CreatedAt {  get; set; }
+        public enum Status { get; set;  }
+    }
+}
---
+++ ProjektDB/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjektDB.Models
+{
+    public class Users
+    {
+        public Users() { }
+        public int Id { get; set; }
+        public string Username {  get; set; }  
+        public string Password { get; set; }
+    }
+}
