a71f156190212856591bb116591c75a0a6d22ca0
Ebba Högdahl <id22ehl@cs.umu.se>
2024-12-19 10:53:41 +0100

    Added and finished Boards modell

---
+++ ProjektDB/Models/Boards.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjektDB.Models
+{
+    public class Boards
+    {
+        public Boards() { }
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public int UserId { get; set; }
+        public int SizeX { get; set; }
+        public int SizeY { get; set; }
+    }
+}
