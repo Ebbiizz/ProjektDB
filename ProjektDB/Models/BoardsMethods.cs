3e48439ca1b9a20c58f75f8af12380b45efb0298
Ebba Högdahl <id22ehl@cs.umu.se>
2025-01-09 13:45:06 +0100

    Created BoardMethods and made CreateBoard

---
+++ ProjektDB/Models/BoardsMethods.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ProjektDB.Models
+{
+    public class BoardsMethods
+    {
+        public BoardsMethods() { }
+        public bool CreateBoard(int gameId, int userId, out string errormsg)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Server=tcp:sankaskepp.database.windows.net,1433;Initial Catalog=SankaSkepp;Persist Security Info=False;User ID=skeppadmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string sqlstring = "Insert Into Boards (GameId, UserId, SizeX, SizeY) Values (@GameId, @UserId, @SizeX, @SizeY)";
+            SqlCommand sqlCommand = new SqlCommand(sqlstring, sqlConnection);
+            sqlCommand.Parameters.Add("GameId", System.Data.SqlDbType.Int).Value = gameId;
+            sqlCommand.Parameters.Add("UserId", System.Data.SqlDbType.Int).Value = userId;
+            sqlCommand.Parameters.Add("SizeX", System.Data.SqlDbType.Int).Value = 10;
+            sqlCommand.Parameters.Add("SizeY", System.Data.SqlDbType.Int).Value = 10;
+
+            try
+            {
+                sqlConnection.Open();
+                int i = 0;
+                i = sqlCommand.ExecuteNonQuery();
+                if (i == 1)
+                {
+                    errormsg = "";
+                }
+                else
+                {
+                    errormsg = "Insert command failed";
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errormsg = ex.Message;
+                return false;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
