7b2fce38a9189786948a4942b9b534d93a3cb924
Ebba Högdahl <id22ehl@cs.umu.se>
2025-01-11 15:42:43 +0100

    Created place ship

--- ProjektDB/Models/ShipsMethods.cs
+++ ProjektDB/Models/ShipsMethods.cs
@@ -5,40 +5,49 @@ namespace ProjektDB.Models
     public class ShipsMethods
     {
         public ShipsMethods() { }
-        public bool PlaceShip(int BoardId, int startX, int startY, int endX, int endY, ShipType shipType, out string errormsg)
+        public bool PlaceShip(int boardId, int startX, int startY, int endX, int endY, ShipType shipType, out string errormsg)
         {
-            SqlConnection sqlConnection = new SqlConnection();
-            sqlConnection.ConnectionString = "Server=tcp:sankaskepp.database.windows.net,1433;Initial Catalog=SankaSkepp;Persist Security Info=False;User ID=skeppadmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            string sqlstring = "Insert Into Boards (GameId, UserId, SizeX, SizeY) Values (@GameId, @UserId, @SizeX, @SizeY)";
-            SqlCommand sqlCommand = new SqlCommand(sqlstring, sqlConnection);
-            sqlCommand.Parameters.Add("GameId", System.Data.SqlDbType.Int).Value = gameId;
-            sqlCommand.Parameters.Add("UserId", System.Data.SqlDbType.Int).Value = userId;
-            sqlCommand.Parameters.Add("SizeX", System.Data.SqlDbType.Int).Value = 10;
-            sqlCommand.Parameters.Add("SizeY", System.Data.SqlDbType.Int).Value = 10;
-
-            try
+            if (0 < startX && startX < 11 && 0 < startY && startY < 11 && 0 < endX && endX < 11 && 0 < endX && endY < 11)
             {
-                sqlConnection.Open();
-                int i = 0;
-                i = sqlCommand.ExecuteNonQuery();
-                if (i == 1)
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = "Server=tcp:sankaskepp.database.windows.net,1433;Initial Catalog=SankaSkepp;Persist Security Info=False;User ID=skeppadmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                string sqlstring = "Insert Into Ships (BoardId, ShipType, StartX, StartY, EndX, EndY) Values (@BoardId, @ShipType, @StartX, @StartY, @EndX, @EndY)";
+                SqlCommand sqlCommand = new SqlCommand(sqlstring, sqlConnection);
+                sqlCommand.Parameters.Add("BoardId", System.Data.SqlDbType.Int).Value = boardId;
+                sqlCommand.Parameters.Add("ShipType", System.Data.SqlDbType.Int).Value = shipType;
+                sqlCommand.Parameters.Add("StartX", System.Data.SqlDbType.Int).Value = startX;
+                sqlCommand.Parameters.Add("StartY", System.Data.SqlDbType.Int).Value = startY;
+                sqlCommand.Parameters.Add("EndX", System.Data.SqlDbType.Int).Value = endX;
+                sqlCommand.Parameters.Add("EndY", System.Data.SqlDbType.Int).Value = endY;
+                try
                 {
-                    errormsg = "";
+                    sqlConnection.Open();
+                    int i = 0;
+                    i = sqlCommand.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        errormsg = "";
+                    }
+                    else
+                    {
+                        errormsg = "Insert command failed";
+                    }
+                    return true;
                 }
-                else
+                catch (Exception ex)
                 {
-                    errormsg = "Insert command failed";
+                    errormsg = ex.Message;
+                    return false;
                 }
-                return true;
+                finally
+                {
+                    sqlConnection.Close();
+                }
             }
-            catch (Exception ex)
+            else
             {
-                errormsg = ex.Message;
+                errormsg = "invalid values";
                 return false;
-            }
-            finally
-            {
-                sqlConnection.Close();
             }
         }
     }
