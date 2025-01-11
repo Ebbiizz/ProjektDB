0a2290f952a5509e0fe8bec5d6778d0a87651087
Ebba Högdahl <id22ehl@cs.umu.se>
2025-01-11 15:31:15 +0100

    Added GetBoard

--- ProjektDB/Models/BoardsMethods.cs
+++ ProjektDB/Models/BoardsMethods.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace ProjektDB.Models
 {
@@ -35,6 +36,54 @@ namespace ProjektDB.Models
             {
                 errormsg = ex.Message;
                 return false;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        Boards GetBoard(int gameId,int userId, out string errormsg)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Server=tcp:sankaskepp.database.windows.net,1433;Initial Catalog=SankaSkepp;Persist Security Info=False;User ID=skeppadmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string sqlstring = "Select * From Boards Where GameId = @GameId and UserId = @UserId";
+            SqlCommand sqlCommand = new SqlCommand(sqlstring, sqlConnection);
+            sqlCommand.Parameters.Add("GameId", System.Data.SqlDbType.Int).Value = gameId;
+            sqlCommand.Parameters.Add("UserId", System.Data.SqlDbType.Int).Value = userId;
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataSet dataSet = new DataSet();
+            Boards board = new Boards();
+            try
+            {
+                sqlConnection.Open();
+                adapter.Fill(dataSet, "Board");
+                int i = 0;
+                int count = 0;
+                count = dataSet.Tables["Board"].Rows.Count;
+                if (count > 0)
+                {
+                    while (i < count)
+                    {
+                        board.Id = Convert.ToUInt16(dataSet.Tables["Board"].Rows[i]["Id"]);
+                        board.GameId = Convert.ToUInt16(dataSet.Tables["Board"].Rows[i]["GameId"]);
+                        board.SizeX = Convert.ToUInt16(dataSet.Tables["Board"].Rows[i]["SizeX"]);
+                        board.SizeY = Convert.ToUInt16(dataSet.Tables["Board"].Rows[i]["SizeY"]);
+
+                        i++;
+                    }
+                    errormsg = "";
+                    return board;
+                }
+                else
+                {
+                    errormsg = "No active games are fetched";
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                errormsg = e.Message;
+                return null;
             }
             finally
             {
